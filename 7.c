ae8047a9e5baa29665c2a44ada4c38eb7a8f6048
Al.Integra <67740694+otintegra@users.noreply.github.com>
2023-05-05 23:40:12 +0300

    Update 7.c

--- 7.c
+++ 7.c
@@ -2,16 +2,22 @@
 
 void main() {
     int num;
+    // Ввод числа
     scanf("%d", &num);
+    // Создание массива заданной длины и счетчиков
     int digits[num], countZero = 0, countPlus = 0,  countMin = 0;
     {
+        // Цикл ввода чисел для подсчета
         while (num > 0)
         {
+            // Ввод чисел
             scanf("%d", &digits[--num]);
+            // Подсчет по условиям
             if (digits[num] == 0) countZero++;
             if (digits[num] > 0) countPlus++;
             if (digits[num] < 0) countMin++;
-        }     
-            printf("%d %d %d", countZero, countPlus,  countMin);
+        }
+        // Вывод результата
+        printf("%d %d %d", countZero, countPlus,  countMin);
     }
 }
